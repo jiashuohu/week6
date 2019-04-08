5a79b98130037687744cb9dab8e5a1598fee7431
大碩 <loliconwwwwww@gmail.com>
2019-04-08 19:13:48 +0800

    css commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,41 @@
 let path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-    entry: './assets/js/script.js',
-    output: {
-        path: path.join(__dirname, './dist'),
-        filename: 'bundle.js',
-        //publicPath: './dist/'
+    entry:'./assets/js/script.js',
+    output:{
+        path:path.join(__dirname,'./dist'),
+        filename:'bundle.js',
+        //publicPath:'./dist/'
     },
     module:{
-        rules: [
-            
+        rules:[
             {
-                test: /\.(jpe?g|png|gif|svg)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit:400000,
-                            outputPath: './images'
-                        }
-                    },
-                    'image-webpack-loader'
-                ]
-            }
-        ]
-    }
+                use: ExtractTextPlugin.extract({
+                  use: "css-loader"
+                }),
+                test: /\.css$/,
+              },
+        {
+            test:/\.(jpe?g|png|gif|svg)$/,
+            use:[
+                {
+                loader:'url-loader',
+                options:{
+                    limit:400000,
+                    outputPath:'./images',
+                    publicPath: './images'
+                }
+                },
+                'image-webpack-loader'
+        
+    ]
 }
+
+]
+},
+plugins: [
+    new ExtractTextPlugin('./css/style.css'),
+    
+  ]
+  }

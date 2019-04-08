1ef3051364970be172cd1fae23c133bfa8fa04e0
大碩 <loliconwwwwww@gmail.com>
2019-04-09 00:01:22 +0800

    postcss commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,36 @@
+import"../css/fonts/PT-Sans-Bold.eot";
+import"../css/fonts/PT-Sans-Bold.svg";
+import"../css/fonts/PT-Sans-Bold.ttf";
+import"../css/fonts/PT-Sans-Bold.woff";
+import"../css/fonts/PT-Sans-BoldItalic.eot";
+import"../css/fonts/PT-Sans-BoldItalic.svg";
+import"../css/fonts/PT-Sans-BoldItalic.ttf";
+import"../css/fonts/PT-Sans-BoldItalic.woff";
+import"../css/fonts/PT-Sans-Italic.eot";
+import"../css/fonts/PT-Sans-Italic.svg";
+import"../css/fonts/PT-Sans-Italic.ttf";
+import"../css/fonts/PT-Sans-Italic.woff";
+import"../css/fonts/PT-Sans.eot";
+import"../css/fonts/PT-Sans.svg";
+import"../css/fonts/PT-Sans.ttf";
+import"../css/fonts/PT-Sans.woff";
 import $ from 'jquery';
 import 'bootstrap';
+import '../css/style.css';
 import b from '../images/bird.jpg';
 import s from '../images/squirrel.jpg';
-import 'bootstrap';
-import '../css/style.css';
+import'../css/style.css';
 
 $('#bird').attr("src",b);
 $('#squirrel').attr("src",s);
 
-
 $(document).ready(function () {
- // when a hanburger menu is clicked
- let $ul = $(".nav__list"),
-   $menu = $(".nav__menu");
+	// when a hanburger menu is clicked
+	let $ul = $(".nav__list"),
+		 $menu = $(".nav__menu");
 
- $($menu).click(function () {
-  // toggle menu-click Class
-  $ul.toggleClass("menu-click");
- }); // end click event handler
+	$($menu).click(function () {
+		// toggle menu-click Class
+		$ul.toggleClass("menu-click");
+	}); // end click event handler
 });
